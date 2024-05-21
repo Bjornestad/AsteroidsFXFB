9f4f62e6cf95bdce5e70bc69b6242c901bacb6cd
Bjornestad <frederikbjornestad@gmail.com>
2024-05-21 17:02:07 +0200

    split spring

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/ModuleConfig.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/ModuleConfig.java
@@ -6,14 +6,24 @@ import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.lang.module.ModuleDescriptor;
+import java.lang.module.ModuleFinder;
+import java.lang.module.ModuleReference;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.ServiceLoader;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
 @Configuration
 public class ModuleConfig {
+
+    private static ModuleLayer pluginlayer;
+
     public ModuleConfig() {
+        createModuleLayer();
     }
     @Bean(name = "GameBean")
     public Game game() {
@@ -30,6 +40,25 @@ public class ModuleConfig {
     @Bean(name = "PostEntityProcessingBean")
     public List<IPostEntityProcessingService> postEntityProcessingServices() {
         return ServiceLoader.load(IPostEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+    private static void createModuleLayer() {
+        Path pluginsDir = Paths.get("plugins");
+
+        ModuleFinder pluginsFinder = ModuleFinder.of(pluginsDir);
+
+        List<String> plugins = pluginsFinder
+                .findAll()
+                .stream()
+                .map(ModuleReference::descriptor)
+                .map(ModuleDescriptor::name)
+                .collect(Collectors.toList());
+
+        java.lang.module.Configuration pluginsConfiguration = ModuleLayer
+                .boot()
+                .configuration()
+                .resolve(pluginsFinder, ModuleFinder.of(), plugins);
+
+        pluginlayer = ModuleLayer.boot().defineModulesWithOneLoader(pluginsConfiguration, ClassLoader.getSystemClassLoader());
     }
 }
 
